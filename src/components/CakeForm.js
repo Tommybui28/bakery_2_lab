31c82cbcbfeed356805e5b215df31ce027e4bf64
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-21 15:16:06 +0000

    Validation added and JSX initialised

--- src/components/CakeForm.js
+++ src/components/CakeForm.js
@@ -1,5 +1,39 @@
-import {useState} from "react";
+import {useDebugValue, useState} from "react";
 const CakeForm =({cakes, newCake}) => {
 
+    const [recipeName, setRecipeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [rating, setRating] = useState("");
+
+    useState("");
+    const[error, setError] = useState("");
     
-}
+    const handleValidation = () => {
+        let validation = true;
+        let errorMessage ="";
+        
+        if (cakes.find((cake) => cake.recipeName === recipeName)) {
+            errorMessage = "This Recipe already exists";
+            validation = false;
+        }
+        setError(errorMessage);
+        return errorMessage !== "";
+    }
+    
+
+
+
+    return (
+       <>
+        <h2>Add Recipe Here!</h2>
+        <form onSubmit={handleSubmit} />
+            <input 
+                type="text"
+                name="recipeName"
+                placeholder="Enter recipe name here"
+                value={}
+            />
+        </>
+    );
+    
+};
